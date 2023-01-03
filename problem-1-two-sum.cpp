2fa96757e35c3b1f592891f62b43d676dfe814ad
Trâm T <97485876+tram-tr@users.noreply.github.com>
2023-01-02 20:12:07 -0600

    Update problem-1-two-sum.cpp

--- problem-1-two-sum.cpp
+++ problem-1-two-sum.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        # O(n)
+       
         unordered_map<int, int>hashMap;
         vector<int> ans {0,0};
         int diff = 0;
-
+        
+        # target = ans[0] + ans[1] => ans[0] = target - ans[1]
         for (size_t i = 0; i < nums.size(); i++) {
             if (hashMap.count(nums[i]) > 0) {
                 ans[0] = hashMap[nums[i]];
