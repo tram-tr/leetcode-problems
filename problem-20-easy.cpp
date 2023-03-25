bf4da198ecfe7ac0fee918d5b82f8e1780734265
Trâm T <97485876+tram-tr@users.noreply.github.com>
2023-03-24 19:22:06 -0700

    Create problem-20-easy.cpp

---
+++ problem-20-easy.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> brackets;
+        for (auto &c : s) {
+            if (c == '(' || c == '{' || c == '[')
+                brackets.push(c);
+            else {
+                if (brackets.empty())
+                    return false;
+                char top = brackets.top();
+                brackets.pop();
+                if ((c == ')' && top != '(') || (c == ']' && top != '[') || (c == '}' && top != '{'))
+                    return false;
+            }
+
+        }
+       return brackets.empty();
+    }
+};
