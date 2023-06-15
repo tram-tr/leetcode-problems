45a1d447a3c6935d23066078d297f28beb4dfbba
Trâm T <97485876+tram-tr@users.noreply.github.com>
2023-06-14 23:00:03 -0700

    add problem-110-easy.cpp

---
+++ problem-110-easy.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // Bottom Up Solution
+    // Time: O(N)
+    int dfs(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        if (left == -1 || right == -1 || abs(left-right) > 1)
+            return -1;
+        else
+            return 1 + max(left, right);
+    }
+    bool isBalanced(TreeNode* root) {
+        if (root == nullptr)
+            return true;
+        return dfs(root) != -1;
+    }
+};
