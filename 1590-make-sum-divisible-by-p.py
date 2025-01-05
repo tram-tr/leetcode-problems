a72753f7c3dc1121d0e258477ee2e730e2722d56
Trâm Trịnh <97485876+tram-tr@users.noreply.github.com>
2025-01-04 23:02:18 -0600

    Update 1590-make-sum-divisible-by-p.py

--- 1590-make-sum-divisible-by-p.py
+++ 1590-make-sum-divisible-by-p.py
@@ -22,17 +22,15 @@ class Solution(object):
             if (sum_n - s) % p == 0: # 0..i
                 length = min(length, i + 1)
             
-            if (s % p - k) % p in prefix_sum: # j..i
-                j = prefix_sum[(s % p - k) % p] + 1
+            mod = (s % p - k) % p
+            if mod in prefix_sum: # j..i
+                j = prefix_sum[mod] + 1
                 length = min(length, i - j + 1)
             
-            if s % p not in prefix_sum:
-                prefix_sum[s % p] = i
             prefix_sum[s % p] = i
     
         if length < len(nums):
             return length
         else:
             return -1
-
         
