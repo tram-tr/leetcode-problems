1f52891f9aa3144da91a0640e70a909e0c0c703e
Trâm Trịnh <97485876+tram-tr@users.noreply.github.com>
2025-01-05 00:47:17 -0600

    Update 525-contiguous-array.py

--- 525-contiguous-array.py
+++ 525-contiguous-array.py
@@ -2,15 +2,13 @@
 
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        prefix_sum = {}
+        prefix_sum = {0: -1}
         n = len(nums)
         s = 0
         res = 0
 
         for i in range(n):
             s += nums[i] if nums[i] == 1 else -1
-            if s == 0:
-                res = max(res, i + 1)
             if s in prefix_sum:
                 j = prefix_sum[s] + 1
                 res = max(res, i - j + 1)
