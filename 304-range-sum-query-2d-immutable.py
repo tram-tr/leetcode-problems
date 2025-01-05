51d2c7e71d80244d25b6375f71461d7b01ca0199
Trâm Trịnh <97485876+tram-tr@users.noreply.github.com>
2025-01-04 23:56:44 -0600

    Update 304-range-sum-query-2d-immutable.py

--- 304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable.py
@@ -8,12 +8,14 @@ class NumMatrix(object):
         """
         n = len(matrix)
         m = len(matrix[0])
-        self.prefix_sum = [[0] * m for _ in range(n)]
+        self.prefix_sum = [[0] * (m + 1) for _ in range(n + 1)]
         
-        for i in range(n):
-            self.prefix_sum[i][0] = matrix[i][0]
-            for j in range(1, m):
-                self.prefix_sum[i][j] = self.prefix_sum[i][j-1] + matrix[i][j]
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                self.prefix_sum[i][j] = (self.prefix_sum[i - 1][j] +
+                                self.prefix_sum[i][j - 1] -
+                                self.prefix_sum[i - 1][j - 1] +
+                                matrix[i - 1][j - 1])
 
     def sumRegion(self, row1, col1, row2, col2):
         """
@@ -23,17 +25,10 @@ class NumMatrix(object):
         :type col2: int
         :rtype: int
         """
-        s = 0
-        i = row1
-        while i <= row2:
-            if col1 != 0:
-                s += self.prefix_sum[i][col2] - self.prefix_sum[i][col1 - 1]
-            else:
-                s += self.prefix_sum[i][col2]
-
-            i += 1
-
-        return s
+        return (self.prefix_sum[row2 + 1][col2 + 1] -
+                self.prefix_sum[row1][col2 + 1] -
+                self.prefix_sum[row2 + 1][col1] +
+                self.prefix_sum[row1][col1])
 
 
 # Your NumMatrix object will be instantiated and called as such:
