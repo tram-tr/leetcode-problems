51095923c6415cccf4a39f5a3d4cfa647c1b066d
Trâm Trịnh <97485876+tram-tr@users.noreply.github.com>
2025-01-11 19:38:55 -0500

    Create 1004-max-consecutive-ones-iii.py

---
+++ 1000-1099/1004-max-consecutive-ones-iii.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/max-consecutive-ones-iii/
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        res = 0
+        left = 0
+        count = 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                count += 1
+            while count > k:
+                if nums[left] == 0:
+                    count -= 1
+                left += 1
+            res = max(res, right - left + 1)
+        return res
