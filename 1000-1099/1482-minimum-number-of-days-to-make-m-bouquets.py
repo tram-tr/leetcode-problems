1329f35c877dbea650f4a55e3062531a1e2e2fb3
Trâm Trịnh <97485876+tram-tr@users.noreply.github.com>
2025-01-18 22:17:36 -0500

    Create 1482-minimum-number-of-days-to-make-m-bouquets.py

---
+++ 1000-1099/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/description/
+
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def can_load(max_w):
+            count = 1
+            curr_w = 0
+            for w in weights:
+                if curr_w + w > max_w:
+                    count += 1
+                    curr_w = 0
+                curr_w += w
+            
+            return count <= days
+                
+        left = max(weights)
+        right = sum(weights)
+
+        while left < right:
+            mid = left + (right - left) // 2
+            if can_load(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
