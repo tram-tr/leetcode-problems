4094c551946768f0821eaa9c7862e04676034052
Trâm Trịnh <97485876+tram-tr@users.noreply.github.com>
2025-02-10 00:51:09 -0500

    Create 572-subtree-of-another-tree.py

---
+++ 0500-0599/572-subtree-of-another-tree.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/subtree-of-another-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if root is None:
+            return False
+
+        def dfs(root1, root2):
+            if root1 is None and root2 is None:
+                return True
+            if root1 is None or root2 is None:
+                return False
+
+            return root1.val == root2.val and dfs(root1.left, root2.left) and dfs(root1.right, root2.right)
+            
+
+        return dfs(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        
