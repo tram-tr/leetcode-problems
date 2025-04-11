6a781a664a9e5f79c6decf58e2a6c847c4eb53f6
Trâm Trịnh <97485876+tram-tr@users.noreply.github.com>
2025-04-11 02:43:22 -0400

    Create 3394-check-if-grid-can-be-cut-into-sections.py

---
+++ 3300-3399/3394-check-if-grid-can-be-cut-into-sections.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/check-if-grid-can-be-cut-into-sections/
+
+class Solution(object):
+    def checkValidCuts(self, n, rectangles):
+        return self.isValidCut(rectangles, 0, 0, 2) or self.isValidCut(rectangles, 1, 1, 3)
+
+    def isValidCut(self, rectangles, sort_index, start, end):
+        rectangles.sort(key=lambda x: x[sort_index])
+        
+        current_start = rectangles[0][start]
+        current_end = rectangles[0][end]
+        intervals = 0
+
+        for rect in rectangles:
+            if rect[start] < current_end:
+                current_end = max(rect[end], current_end)
+            else:
+                intervals += 1
+                current_start = rect[start]
+                current_end = rect[end]
+        
+        intervals += 1
+        return intervals > 2
