0c5ecfc250f8b6bde9ea383c8f20f80f423e03aa
Trâm Trịnh <97485876+tram-tr@users.noreply.github.com>
2025-04-11 02:33:04 -0400

    Create 1207-unique-number-of-occurrences.py

---
+++ 1200-1299/1207-unique-number-of-occurrences.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/unique-number-of-occurrences
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        counter = Counter(arr)
+        return len(set(counter.values())) == len(counter)
