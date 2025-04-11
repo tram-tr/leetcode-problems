5bc98e989063379e3bedd1118a741698ade79657
Trâm Trịnh <97485876+tram-tr@users.noreply.github.com>
2025-04-11 02:20:21 -0400

    Create 136-single-number.py

---
+++ 0100-0199/136-single-number.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/single-number
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for i in range(1, len(nums)):
+            nums[0] ^= nums[i]
+        return nums[0]
