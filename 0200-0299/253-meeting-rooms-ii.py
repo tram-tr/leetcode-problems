4ef5dc6cf41f45577ce2d75ae10dc672d2947f18
Trâm Trịnh <97485876+tram-tr@users.noreply.github.com>
2025-04-11 04:35:41 -0400

    Update 253-meeting-rooms-ii.py

--- 0200-0299/253-meeting-rooms-ii.py
+++ 0200-0299/253-meeting-rooms-ii.py
@@ -1,18 +1,36 @@
 # https://leetcode.com/problems/meeting-rooms-ii
 
-from heapq import heappush, heappop
+# from heapq import heappush, heappop
 
-class Solution:
-    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
+# class Solution:
+#     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+#         intervals.sort()
 
-        cur = []
-        rooms = 0
-        for start, end in intervals:
-            if not cur or cur[0] > start:
-                rooms += 1
-            else:
-                heappop(cur)
-            heappush(cur, end)
+#         cur = []
+#         rooms = 0
+#         for start, end in intervals:
+#             if not cur or cur[0] > start:
+#                 rooms += 1
+#             else:
+#                 heappop(cur)
+#             heappush(cur, end)
 
-        return rooms
+#         return rooms
+
+class Solution(object):
+  def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+    meetings = []
+    for i in intervals:
+      meetings.append((i.start, 1))
+      meetings.append((i.end, 0))
+    meetings.sort()
+    ans = 0
+    count = 0
+    for meeting in meetings:
+      if meeting[1] == 1:
+        count += 1
+      else:
+        count -= 1
+      ans = max(ans, count)
+    return ans
+
