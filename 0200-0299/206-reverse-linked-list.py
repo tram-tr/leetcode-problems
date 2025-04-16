3b27f42317fdc694139e251ddb627f30cbacb705
Trâm Trịnh <97485876+tram-tr@users.noreply.github.com>
2025-04-15 21:51:51 -0400

    Update 206-reverse-linked-list.py

--- 0200-0299/206-reverse-linked-list.py
+++ 0200-0299/206-reverse-linked-list.py
@@ -7,20 +7,71 @@
 #         self.next = next
 
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
+    # def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     if not head:
+    #         return None
            
-        prev = None
+    #     # use two pointers prev and curr
+    #     # initially prev is None and curr points to head
+    #     # let next to be the next node of curr before reversing curr, next = curr.next
+    #     # set curr.next = prev
+    #     # set prev = curr and curr = next
+
+    #     # head -> [1] -> [2] -> [3] -> None
+    #     # curr -> [1] -> [2] -> [3] -> None
+    #     # prev -> None
+
+    #     # nxt -> [2] -> [3] -> None
+    #     # curr.next -> None => [1] -> None
+    #     # prev -> [1] -> None
+    #     # curr -> [2] -> [3] -> None
+
+    #     # nxt -> [3] -> None
+    #     # curr.next -> [1] -> None => [2] -> [1] -> None
+    #     # prev -> [2] -> [1] -> None
+    #     # curr -> [3] -> None
+
+    #     # nxt -> None
+    #     # curr.next -> [2] => [3] -> [2] -> [1] -> None
+    #     # prev -> [3] -> [2] -> [1] -> None
+    #     # curr -> None
+
+    #     prev = None
+    #     curr = head
+    #     nxt = None
+
+
+    #     while curr:
+    #         nxt = curr.next # save the next node
+    #         curr.next = prev # reverse the link
+    #         prev = curr # move prev forward
+    #         curr = nxt # move curr forward
+
+
+    #     return prev
+
+    # def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     if not head or not head.next:
+    #         return head
+
+    #     # do the reverse operation for head.next
+    #     # set head.next.next = head
+    #     # set head.next = None
+    #     new_head = self.reverseList(head.next)
+    #     head.next.next = head # head.next is end of the newly reversed list
+    #     head.next = None
+
+    #     return new_head
+
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode() # dummny.next -> None
         curr = head
-        next = None
-
-
         while curr:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
+            nxt = curr.next
+            curr.next = dummy.next
+            dummy.next = curr
+            curr = nxt
 
+        return dummy.next
 
-        return prev
+        
